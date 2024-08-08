e01fa3ea08166a713b0c3e1f1c3ffb9b0076c410
Tino Chirozvi <134059951+tinothecreator@users.noreply.github.com>
2024-08-08 08:35:47 +0200

    making my first commit - Tino

--- Soldier.cpp
+++ Soldier.cpp
@@ -1,14 +1,5 @@
 #include "Soldier.h"
 
-Memento* Soldier::militusMemento() {
-	// TODO - implement Soldier::militusMemento
-	throw "Not yet implemented";
-}
-
-void Soldier::vivificaMemento(Memento* mem) {
-	// TODO - implement Soldier::vivificaMemento
-	throw "Not yet implemented";
-}
 
 Soldier::Soldier() {
 	// TODO - implement Soldier::Soldier
@@ -19,3 +10,15 @@ Soldier::Soldier(int int_healthPerSoldier, int int_damagePerSoldier, int int_def
 	// TODO - implement Soldier::Soldier
 	throw "Not yet implemented";
 }
+
+Memento* Soldier::militusMemento() {
+	// TODO - implement Soldier::militusMemento
+	throw "Not yet implemented";
+}
+
+void Soldier::vivificaMemento(Memento* mem) {
+	// TODO - implement Soldier::vivificaMemento
+	throw "Not yet implemented";
+}
+
+
